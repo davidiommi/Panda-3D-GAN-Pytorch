d7503e3937654f010c7898416e5b2eb79619ab02
David Iommi <38066083+canviobasic@users.noreply.github.com>
2020-11-04 13:30:39 +0100

    Update cropNiftiForGan.m

--- Matlab scripts/cropNiftiForGan.m
+++ Matlab scripts/cropNiftiForGan.m
@@ -15,32 +15,26 @@
 %------------------------------------------------------------------------%
 %                           Program start
 %------------------------------------------------------------------------%
-function [] = cropNiftiForGan(CNGinputs)
-
+function [] = cropNiftiForGan_spm(CNGinputs)
 % Hard-coded variables.
-
 xyzDim=[128 128 128]; % dimensions to be cropped
 cropRange=108;  % crop region 
-
 % create the folder where the cropped images will be stored.
-
 cd(CNGinputs.where2Store)
 splitFiles=regexp(CNGinputs.path2Nifti,filesep,'split');
 convertedFolder=[splitFiles{end},'-','cropped'];
 mkdir(convertedFolder); % creating the converted folder for storing
 path2ConvFolder=[CNGinputs.where2Store,filesep,convertedFolder];
-
 % load the nifti files.
-
 cd(CNGinputs.path2Nifti)
 niftiFiles=dir('*.nii');
-parfor lp=1:length(niftiFiles)
-    hdrInfo=niftiinfo(niftiFiles(lp).name);
-    hdrInfo.Description = 'cropped for GANS: Internal use only!';
+for lp=1:length(niftiFiles)
+    hdrInfo=spm_vol(niftiFiles(lp).name);
+    hdrInfo.descrip = 'cropped for GANS: Internal use only!';
     croppedFileName=['crpd-',niftiFiles(lp).name];
-    hdrInfo.Filename=croppedFileName;
-    hdrInfo.ImageSize=xyzDim;
-    imgVol=niftiread(niftiFiles(lp).name);
+    hdrInfo.fname=croppedFileName;
+    hdrInfo.dim=xyzDim;
+    imgVol=spm_read_vols(spm_vol((niftiFiles(lp).name)));
     croppedVol=imgVol;
     xMax=size(imgVol,1);
     yMax=size(imgVol,2);
@@ -48,10 +42,9 @@ parfor lp=1:length(niftiFiles)
     croppedVol=croppedVol(cropRange+1:xMax-cropRange,cropRange+1:yMax-cropRange,:);
     emptyVol=zeros([xyzDim(1) xyzDim(2) (xyzDim(3)-size(imgVol,3))]);
     croppedVol=cat(3,croppedVol,int16(emptyVol));
-    niftiwrite(croppedVol,croppedFileName,hdrInfo);
+    croppedVol=uint16(croppedVol);
+    spm_write_vol(hdrInfo,croppedVol);
     disp(['Writing ',croppedFileName,'...']);
     movefile(croppedFileName,path2ConvFolder);
 end
-
-
 end
